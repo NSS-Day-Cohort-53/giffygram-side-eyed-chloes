55adfbb7ed528b3ca443a088c3d559720298fb87
jaypetty <jayshaun2343@gmail.com>
2022-01-05 11:38:31 -0600

    rendered login page

--- src/scripts/data/provider.js
+++ src/scripts/data/provider.js
@@ -10,3 +10,14 @@ const applicationState = {
         displayMessages: false
     }
 }
+
+
+export const fetchUsers = async () => {
+    const response = await fetch(`${apiURL}/users`)
+    const users = await response.json()
+    applicationState.users = users
+}
+
+export const getUsers = () => {
+    return applicationState.users.map(user => ({...user}))
+}
